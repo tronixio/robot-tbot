a915a6d8be3b62a86569ca2e1a6f5a10980b79b7
Tronix I/O <23189430+tronixio@users.noreply.github.com>
2022-02-07 18:42:14 +0100

    Update configuration.s

--- Code/configuration.s
+++ Code/configuration.s
@@ -1,1 +1,159 @@
 ; Configuration Registers.
+CONFIG FOSC=INTOSC
+CONFIG WDTE=OFF
+CONFIG PWRTE=OFF
+CONFIG MCLRE=ON
+CONFIG CP=OFF
+CONFIG BOREN=OFF
+CONFIG CLKOUTEN=ON
+CONFIG IESO=OFF
+CONFIG FCMEN=OFF
+CONFIG WRT=OFF
+CONFIG PPS1WAY=ON
+CONFIG ZCD=OFF
+CONFIG PLLEN=OFF
+CONFIG STVREN=ON
+CONFIG BORV=LO
+CONFIG LPBOR=OFF
+CONFIG LVP=ON
+
+#include <xc.inc>
+; PIC16F1778 - Compile with PIC-AS(v2.35).
+; PIC16F1778 - @8MHz Internal Oscillator.
+; -preset_vec=0000h, -pcinit=0005h.
+; 1 instruction 495ns.
+
+; TBOT - v0.1.
+
+; MCU.RA6 -> OSCILLOSCOPE.PROBE.A
+
+; MCU Definitions.
+; BANKS.
+#define	BANK0   0x0
+#define	BANK1   0x1
+#define	BANK2   0x2
+#define	BANK3   0x3
+#define	BANK4   0x4
+#define	BANK5   0x5
+#define	BANK6   0x6
+#define	BANK7   0x7
+#define	BANK8   0x8
+#define	BANK9   0x9
+#define	BANK10  0xA
+#define	BANK11  0xB
+#define	BANK12  0xC
+#define	BANK13  0xD
+#define	BANK14  0xE
+#define	BANK15  0xF
+#define	BANK16  0x10
+#define	BANK17  0x11
+#define	BANK18  0x12
+#define	BANK19  0x13
+#define	BANK20  0x14
+#define	BANK21  0x15
+#define	BANK22  0x16
+#define	BANK23  0x17
+#define	BANK24  0x18
+#define	BANK25  0x19
+#define	BANK26  0x1A
+#define	BANK27  0x1B
+#define	BANK28  0x1C
+#define	BANK29  0x1D
+
+; Reset Vector.
+PSECT reset_vec,class=CODE,space=0,delta=2
+resetVect:
+    goto    main
+
+; Main.
+PSECT cinit,class=CODE,space=0,delta=2
+main:
+    ; MCU Initialization.
+    ; Internal Oscillator Settings.
+    movlb   BANK1
+    movlw   0b00000110
+    movwf   OSCTUNE
+    movlw   0x70
+    movwf   OSCCON
+    btfss   HFIOFR
+    bra	    $-1
+    ; Ports Settings.
+    ; PORT Data Register.
+    movlb   BANK0
+    movlw   0b00000000
+    movwf   PORTA
+    movlw   0b00000000
+    movwf   PORTB
+    movlw   0b00000000
+    movwf   PORTC
+    movlw   0b00000000
+    movwf   PORTE
+    ; TRIS Data Direction.
+    movlb   BANK1
+    movlw   0b00100000
+    movwf   TRISA
+    movlw   0b00001001
+    movwf   TRISB
+    movlw   0b00000000
+    movwf   TRISC
+    movlw   0b00000000
+    movwf   TRISE
+    ; LATCH Outputs.
+    movlb   BANK2
+    movlw   0b00000000
+    movwf   LATA
+    movlw   0b00000000
+    movwf   LATB
+    movlw   0b00000000
+    movwf   LATC
+    ; ANSEL Analog.
+    movlb   BANK3
+    movlw   0b00000000
+    movwf   ANSELA
+    movlw   0b00001000
+    movwf   ANSELB
+    movlw   0b00000000
+    movwf   ANSELC
+    ; WPU Weak Pull-up.
+    movlb   BANK4
+    movlw   0b00000000
+    movwf   WPUA
+    movlw   0b00000000
+    movwf   WPUB
+    movlw   0b00000000
+    movwf   WPUC
+    movlw   0b00000000
+    movwf   WPUE
+    ; ODCON Open-drain.
+    movlb   BANK5
+    movlw   0b00000000
+    movwf   ODCONA
+    movlw   0b00000000
+    movwf   ODCONB
+    movlw   0b00000000
+    movwf   ODCONC
+    ; SRLCON Slew Rate.
+    movlb   BANK6
+    movlw   0b11111111
+    movwf   SLRCONA
+    movlw   0b11111111
+    movwf   SLRCONB
+    movlw   0b11111111
+    movwf   SLRCONC
+    ; INLVL Input Level.
+    movlb   BANK7
+    movlw   0b00000000
+    movwf   INLVLA
+    movlw   0b00000000
+    movwf   INLVLB
+    movlw   0b00000000
+    movwf   INLVLC
+    ; HIDRVB High Drive.
+    movlb   BANK8
+    movlw   0b00000000
+    movwf   HIDRVB
+
+loop:
+    bra	    $
+
+    END	    resetVect
