404a5f1c3cef31c092f082f212f01959f12ab313
Tronix I/O <23189430+tronixio@users.noreply.github.com>
2022-02-17 13:59:37 +0100

    Update configuration.s

--- Code/configuration/configuration.s
+++ Code/configuration/configuration.s
@@ -23,7 +23,7 @@ CONFIG LVP=ON
 ; -preset_vec=0000h, -pcinit=0005h.
 ; Instruction ~500ns @8MHz.
 
-; TBOT - Configuration.
+; TBOT - MCU & GPIO Configuration.
 ; MCU.RA6 -> OSCILLOSCOPE.PROBE.A
 
 ; MCU Definitions.
