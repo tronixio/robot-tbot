1edab6dbb5e5084b87493d66c21e2cbc1f4cb23a
Tronix I/O <23189430+tronixio@users.noreply.github.com>
2022-02-25 18:43:51 +0100

    Update gp2y0d21yk.s

--- Code/gp2y0d21yk/gp2y0d21yk.s
+++ Code/gp2y0d21yk/gp2y0d21yk.s
@@ -18,7 +18,7 @@ CONFIG LPBOR=OFF
 CONFIG LVP=ON
 
 #include <xc.inc>
-; PIC16F1778 - Compile with PIC-AS(v2.35).
+; PIC16F1778 - Compile with PIC-AS(v2.36).
 ; PIC16F1778 - @8MHz Internal Oscillator.
 ; -preset_vec=0000h, -pintentry=0004h, -pcinit=0005h.
 ; Instruction ~500ns @8MHz.
@@ -78,158 +78,158 @@ delay:  DS  2
 ; Reset Vector.
 PSECT reset_vec,class=CODE,space=0,delta=2
 resetVect:
-    GOTO    main
+GOTO    main
 
 ; ISR Vector.
 PSECT intentry,class=CODE,space=0,delta=2
 interruptVector:
-    GOTO    isr
+GOTO    isr
 
 ; Main.
 PSECT cinit,class=CODE,space=0,delta=2
 main:
-    ; MCU Initialization.
-    ; Internal Oscillator Settings.
-    MOVLB   BANK1
-    MOVLW   0b00000110
-    MOVWF   OSCTUNE
-    MOVLW   0x70
-    MOVWF   OSCCON
-    BTFSS   HFIOFR
-    BRA	    $-1
-    ; Ports Settings.
-    ; PORT Data Register.
-    MOVLB   BANK0
-    MOVLW   0b00000000
-    MOVWF   PORTA
-    MOVLW   0b00000000
-    MOVWF   PORTB
-    MOVLW   0b00000000
-    MOVWF   PORTC
-    MOVLW   0b00000000
-    MOVWF   PORTE
-    ; TRIS Data Direction.
-    MOVLB   BANK1
-    MOVLW   0b00100000
-    MOVWF   TRISA
-    MOVLW   0b00001001
-    MOVWF   TRISB
-    MOVLW   0b00000100
-    MOVWF   TRISC
-    MOVLW   0b00000000
-    MOVWF   TRISE
-    ; LATCH Outputs.
-    MOVLB   BANK2
-    MOVLW   0b00000000
-    MOVWF   LATA
-    MOVLW   0b00000000
-    MOVWF   LATB
-    MOVLW   0b00000000
-    MOVWF   LATC
-    ; ANSEL Analog.
-    MOVLB   BANK3
-    MOVLW   0b00000000
-    MOVWF   ANSELA
-    MOVLW   0b00001000
-    MOVWF   ANSELB
-    MOVLW   0b00000000
-    MOVWF   ANSELC
-    ; WPU Weak Pull-up.
-    MOVLB   BANK4
-    MOVLW   0b00000000
-    MOVWF   WPUA
-    MOVLW   0b00000000
-    MOVWF   WPUB
-    MOVLW   0b00000100
-    MOVWF   WPUC
-    MOVLW   0b00000000
-    MOVWF   WPUE
-    ; ODCON Open-drain.
-    MOVLB   BANK5
-    MOVLW   0b00000000
-    MOVWF   ODCONA
-    MOVLW   0b00000000
-    MOVWF   ODCONB
-    MOVLW   0b00000000
-    MOVWF   ODCONC
-    ; SRLCON Slew Rate.
-    MOVLB   BANK6
-    MOVLW   0b11111111
-    MOVWF   SLRCONA
-    MOVLW   0b11111111
-    MOVWF   SLRCONB
-    MOVLW   0b11111111
-    MOVWF   SLRCONC
-    ; INLVL Input Level.
-    MOVLB   BANK7
-    MOVLW   0b00000000
-    MOVWF   INLVLA
-    MOVLW   0b00000000
-    MOVWF   INLVLB
-    MOVLW   0b00000000
-    MOVWF   INLVLC
-    ; HIDRVB High Drive.
-    MOVLB   BANK8
-    MOVLW   0b00000000
-    MOVWF   HIDRVB
+; MCU Initialization.
+; Internal Oscillator Settings.
+MOVLB   BANK1
+MOVLW   0b00000110
+MOVWF   OSCTUNE
+MOVLW   0x70
+MOVWF   OSCCON
+BTFSS   HFIOFR
+BRA	    $-1
+; Ports Settings.
+; PORT Data Register.
+MOVLB   BANK0
+MOVLW   0b00000000
+MOVWF   PORTA
+MOVLW   0b00000000
+MOVWF   PORTB
+MOVLW   0b00000000
+MOVWF   PORTC
+MOVLW   0b00000000
+MOVWF   PORTE
+; TRIS Data Direction.
+MOVLB   BANK1
+MOVLW   0b00000000
+MOVWF   TRISA
+MOVLW   0b10001001
+MOVWF   TRISB
+MOVLW   0b00000100
+MOVWF   TRISC
+MOVLW   0b00000000
+MOVWF   TRISE
+; LATCH Outputs.
+MOVLB   BANK2
+MOVLW   0b00000000
+MOVWF   LATA
+MOVLW   0b00000000
+MOVWF   LATB
+MOVLW   0b00000000
+MOVWF   LATC
+; ANSEL Analog.
+MOVLB   BANK3
+MOVLW   0b00000000
+MOVWF   ANSELA
+MOVLW   0b00001000
+MOVWF   ANSELB
+MOVLW   0b00000000
+MOVWF   ANSELC
+; WPU Weak Pull-up.
+MOVLB   BANK4
+MOVLW   0b00000000
+MOVWF   WPUA
+MOVLW   0b00000000
+MOVWF   WPUB
+MOVLW   0b00000100
+MOVWF   WPUC
+MOVLW   0b00000000
+MOVWF   WPUE
+; ODCON Open-drain.
+MOVLB   BANK5
+MOVLW   0b00000000
+MOVWF   ODCONA
+MOVLW   0b00000000
+MOVWF   ODCONB
+MOVLW   0b00000000
+MOVWF   ODCONC
+; SRLCON Slew Rate.
+MOVLB   BANK6
+MOVLW   0b11111111
+MOVWF   SLRCONA
+MOVLW   0b11011111
+MOVWF   SLRCONB
+MOVLW   0b11011111
+MOVWF   SLRCONC
+; INLVL Input Level.
+MOVLB   BANK7
+MOVLW   0b00000000
+MOVWF   INLVLA
+MOVLW   0b00000000
+MOVWF   INLVLB
+MOVLW   0b00000000
+MOVWF   INLVLC
+; HIDRVB High Drive.
+MOVLB   BANK8
+MOVLW   0b00000000
+MOVWF   HIDRVB
 
-    ; OPTION REG Settings.
-    ; WPU Enabled.
-    MOVLB   BANK1
-    MOVLW   0b01111111
-    MOVWF   OPTION_REG
+; OPTION REG Settings.
+; WPU Enabled.
+MOVLB   BANK1
+MOVLW   0b01111111
+MOVWF   OPTION_REG
 
-    ; GP2Y0D21 Enable.
-    MOVLB   BANK2
-    BSF	    LATC, GP2Y0D21_ENABLE
-    ; Wait ~60ms.
-    CALL    _delay
-    ; IOC Settings.
-    MOVLB   BANK7
-    BSF	    IOCCP, GP2Y0D21_OUT
-    BSF	    IOCCN, GP2Y0D21_OUT
-    CLRF    IOCCF
+; GP2Y0D21 Enable.
+MOVLB   BANK2
+BSF	    LATC, GP2Y0D21_ENABLE
+; Wait ~60ms.
+CALL    _delay
+; IOC Settings.
+MOVLB   BANK7
+BSF	    IOCCP, GP2Y0D21_OUT
+BSF	    IOCCN, GP2Y0D21_OUT
+CLRF    IOCCF
 
-    ; INTERRUPTS Settings.
-    BSF	    IOCIE
-    BCF	    IOCIF
-    ; INTERRUPTS Enabled.
-    BSF	    GIE
+; INTERRUPTS Settings.
+BSF	    IOCIE
+BCF	    IOCIF
+; INTERRUPTS Enabled.
+BSF	    GIE
 
 loop:
-    BRA	    $
+BRA	    $
 
 ;  Interrupt Service Routine.
 isr:
-    ; Interrupt On Change ?
-    BTFSS   IOCIF
-    RETFIE
-    MOVLB   BANK7
-    BTFSS   IOCCF, GP2Y0D21_OUT
-    RETFIE
-    MOVLB   BANK0
-    BTFSC   LATC, GP2Y0D21_OUT
-    BRA	    $+4
-    MOVLB   BANK2
-    BCF	    LATA, LED_DEBUG
-    BRA	    $+3
-    MOVLB   BANK2
-    BSF	    LATA, LED_DEBUG
-    MOVLB   BANK7
-    BCF	    IOCCF, GP2Y0D21_OUT
-    BCF	    IOCIF
-    RETFIE
+; Interrupt On Change ?
+BTFSS   IOCIF
+RETFIE
+MOVLB   BANK7
+BTFSS   IOCCF, GP2Y0D21_OUT
+RETFIE
+MOVLB   BANK0
+BTFSC   LATC, GP2Y0D21_OUT
+BRA	    $+4
+MOVLB   BANK2
+BCF	    LATA, LED_DEBUG
+BRA	    $+3
+MOVLB   BANK2
+BSF	    LATA, LED_DEBUG
+MOVLB   BANK7
+BCF	    IOCCF, GP2Y0D21_OUT
+BCF	    IOCIF
+RETFIE
 
 ; Functions.
 _delay:
-    MOVLW   160
-    MOVWF   delay + 1
-    MOVLW   255
-    MOVWF   delay
-    DECFSZ  delay, F
-    BRA	    $-1
-    DECFSZ  delay + 1, F
-    BRA	    $-5
-    RETURN
+MOVLW   160
+MOVWF   delay + 1
+MOVLW   255
+MOVWF   delay
+DECFSZ  delay, F
+BRA	    $-1
+DECFSZ  delay + 1, F
+BRA	    $-5
+RETURN
 
-    END	    resetVect
+END	    resetVect
